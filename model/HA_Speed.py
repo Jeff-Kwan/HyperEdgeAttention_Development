674a0e5fed80bae3f884d9c54be674ddcf20494b
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-04-02 16:52:54 +0800

    more checks

--- model/HA_Speed.py
+++ model/HA_Speed.py
@@ -3,6 +3,7 @@ import json
 
 from HAT import HAT_Classifier
 import time
+import gc
 
 
 def measure_performance(model, dummy_input, num_runs=100, autocast=False):
@@ -84,12 +85,23 @@ if __name__ == "__main__":
     model = torch.compile(model)
     measure_performance(model, dummy_input, num_runs=100)
 
+    # Clear everything and reinitialize the model
+    del model
+    torch.cuda.empty_cache()
+    gc.collect()
+    model = HAT_Classifier(config).to(device)
+
     print("\nNow with flash attention and autocast")
+    torch.backends.cudnn.enabled = True
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.allow_tf32 = True
+    torch.set_float32_matmul_precision('medium')
     torch.backends.cuda.enable_flash_sdp(True)
     torch.backends.cuda.enable_mem_efficient_sdp(False)
     torch.backends.cuda.enable_math_sdp(False)
+    model = torch.compile(model)
     measure_performance(model, dummy_input, num_runs=100, autocast=True)
 
     print("\nNow with channels last memory as well")
-    dummy_input = dummy_input.to(memory_format=torch.channels_last).contiguous()
+    dummy_input = dummy_input.contiguous(memory_format=torch.channels_last)
     measure_performance(model, dummy_input, num_runs=100, autocast=True)
