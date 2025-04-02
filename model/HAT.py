c841163c7d7b41a51bc97db10ef2e03c32783710
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-04-03 00:51:54 +0800

    .

--- model/HAT.py
+++ model/HAT.py
@@ -5,7 +5,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-from HyperEdgeAttentionVer2 import HyperEdgeAttention
+from .HyperEdgeAttentionVer2 import HyperEdgeAttention
 
 
 class RMSNormPermute(nn.Module):
