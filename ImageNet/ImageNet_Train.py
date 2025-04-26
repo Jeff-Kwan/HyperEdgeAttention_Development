176d0df15233b40964655635b3f7638b5f4eecd2
Kwan Leong Chit Jeff <informaverse@gmail.com>
2025-04-27 00:29:34 +0800

    sync

--- ImageNet/ImageNet_Train.py
+++ ImageNet/ImageNet_Train.py
@@ -199,14 +199,14 @@ def main():
         num_workers=cpu_workers,
         pin_memory=True,
         persistent_workers=True,
-        prefetch_factor=4,
+        prefetch_factor=2,
     )
     val_loader = DataLoader(
         val_dataset,
         batch_size=batch_size,
         num_workers=cpu_workers,
         pin_memory=True,
-        prefetch_factor=4,
+        prefetch_factor=2,
     )
 
     # Set up loss function, optimizer, and learning rate scheduler
